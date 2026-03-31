bdd2155776d668e4660c815341dd329c814553d1
Potato <66733266+TagniPtto@users.noreply.github.com>
2026-03-31 20:56:58 +0200

    MadeGrabbing
    Can grab rigidbody object

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class Mine : MonoBehaviour
 {
+
+    [SerializeField] public float ExplosionForce = 100.0f;
+    [SerializeField] public float ExplosionRadius = 100.0f;
+
+    private bool isActive;
+    public void SetMineActive()
+    {
+        isActive = true;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        other.GetComponent<CharacterController>().enabled = false;
-        
-        other.GetComponent<Rigidbody>().AddExplosionForce(500f, transform.position, 5f);
+        if (!isActive) return;
+        if (other.CompareTag("Player") || other.CompareTag("Ball"))
+        {
+            other.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce, transform.position, ExplosionRadius);
+        }
     }
 }
--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -2,18 +2,38 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using System.Linq;
 
-
+[System.Serializable]
+public class ObjectSlot
+{
+    public GameObject gameObject;
+    public Transform transform;
+}
 public class CharacterController : MonoBehaviour
 {
     private Rigidbody rb;
 
+    [Header("Movement")]
     public Vector3 m_movementDirection = Vector3.zero;
     public Quaternion m_lookAtDirection = Quaternion.identity;
 
     [Range(.5f, 100.0f)] public float movementSpeed;
     [Range(.5f, 200.0f)] public float maxSpeed;
     [Range(.5f, 10000.0f)] public float jumpForce;
+
+
+
+    [Header("Grabbing")]
+    public float grabbingRange = 1.0f;
+    public LayerMask grabbableLayerMask;
+    public float ThrowingForce = 10.0f;
+
+    public InputActionReference leftGrab; 
+
+    public List<ObjectSlot> slots;
+
     private void Start()
     {
         if(!(rb = transform.GetComponent<Rigidbody>()))
@@ -43,6 +63,11 @@ public class CharacterController : MonoBehaviour
             rb.transform.rotation = m_lookAtDirection;
         }
     }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1f, 0f, 0f, .3f);
+        Gizmos.DrawSphere(transform.position, grabbingRange);
+    }
     public void OnMove(InputAction.CallbackContext context)
     {
         var input = context.ReadValue<Vector2>();
@@ -58,9 +83,54 @@ public class CharacterController : MonoBehaviour
         m_lookAtDirection = Quaternion.LookRotation(new Vector3(input.x, 0, input.y),Vector3.up);
 
     }
-    public void OnThrowProjectile(InputAction.CallbackContext context)
+    public void OnGrabObject(InputAction.CallbackContext context)
     {
-        Debug.Log("Throwing");
+        if (!context.started) return;
+        int index = context.action.name == leftGrab.action.name ? 0 : 1;
 
+        if(index < slots.Count)
+        {
+            if (slots[index].gameObject == null)
+            {
+                GrabNearestRigidBodyObject(index);
+            }
+            else
+            {
+                ThrowObject(index);
+            }
+        }
+    }
+
+    private void ThrowObject(int handIndex) 
+    {
+        var obj = slots[handIndex].gameObject;
+
+        obj.transform.SetParent(null, true);
+        var rb = obj.gameObject.GetComponent<Rigidbody>();
+        var cl = obj.gameObject.GetComponent<Collider>();
+
+        cl.enabled = true;
+        rb.isKinematic = false;
+        rb.AddForce(gameObject.transform.forward * ThrowingForce, ForceMode.Force);
+        slots[handIndex].gameObject = null;
+
+    }
+    private void GrabNearestRigidBodyObject(int handIndex)
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, grabbingRange, grabbableLayerMask);
+        foreach (var hit in hits)
+        {
+            var rb = hit.gameObject.GetComponent<Rigidbody>();
+            var cl = hit.gameObject.GetComponent<Collider>();
+            if (rb != null && cl != null)
+            {
+                hit.gameObject.transform.position = slots[handIndex].transform.position;
+                hit.gameObject.transform.SetParent(slots[handIndex].transform, true);
+                slots[handIndex].gameObject = hit.gameObject;
+                cl.enabled = false;
+                rb.isKinematic = true;
+                return;
+            }
+        }
     }
 }
