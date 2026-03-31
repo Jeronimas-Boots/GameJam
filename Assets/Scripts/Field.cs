94f1a5036fe99adbd533a306e488bf9a677fbbb9
Jorn De Wolf (Laptop) <jorn.de.wolf@student.howest.be>
2026-03-31 18:10:59 +0200

    Add grass texture and dirt texture

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Field : MonoBehaviour
 {
+    [SerializeField] private Material _dirtMaterial;
+    [SerializeField] private Material _grassMaterial;
     [SerializeField]
     private float _sizeMultiplier = 1f;
     private List<GameObject> _field = new List<GameObject>(); 
@@ -38,20 +40,21 @@ public class Field : MonoBehaviour
                 {
                     var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                     cube.transform.SetParent(transform);
-
+                    cube.name = "Cube x" + x + ", y" + y + ", z" + z;
                     // Scale it properly
                     cube.transform.localScale = new Vector3(1 / transform.localScale.x, height, 1 / transform.localScale.z);
                     cube.transform.localScale *= _sizeMultiplier;
+                    var cubeRenderer = cube.GetComponent<Renderer>();
                     if (y == 0)
-                        cube.GetComponent<Renderer>().material.color = new Color(0.10f, 0.75f, 0.10f);
+                        cubeRenderer.material = _grassMaterial;
                     else
-                        cube.GetComponent<Renderer>().material.color = new Color(0.65f, 0.1f, 0.1f);
+                        cubeRenderer.material = _dirtMaterial;
+                    cubeRenderer.material.mainTextureOffset = new Vector2((float)x/10f, (float)z/10f);
 
                     cube.transform.localPosition = new Vector3(
                         x * cube.transform.localScale.x - transform.localScale.x / 2 * cube.transform.localScale.x / _sizeMultiplier + cube.transform.localScale.x / 2,
                         -y * (height/2-height/8),
                         z * cube.transform.localScale.z - transform.localScale.z / 2 * cube.transform.localScale.z / _sizeMultiplier + cube.transform.localScale.z / 2);
-
                     _field.Add(cube);
                 }
             }
