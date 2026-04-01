3d627eb49501f6c282e2e818bcc816f1a08985cd
Jorn De Wolf (Laptop) <jorn.de.wolf@student.howest.be>
2026-04-01 17:10:35 +0200

    Using random to unityengine random

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
 [System.Serializable]
 public class ObjectSlot
